0cb7a81f08262d0b0e46688830d94ad7284407f8
nwellis <nicholas.w.ellis@gmail.com>
2024-03-27 13:13:45 +0800

    feat(web): styling scaffold

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,15 +1,37 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@acme/css-config/tailwind.config";
+import daisyui from 'daisyui';
 
 const fileExtensions = 'html,js,jsx,md,mdx,ts,tsx' as const
 
 const config: Config = {
   presets: [sharedConfig],
-  content: [`./src/**/*.{${fileExtensions}}`],
+  content: [
+    './index.html',
+    `./src/**/*.{${fileExtensions}}`
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      daisyui: {
+        themes: [
+          {
+            mytheme: {
+              "primary": "#be123c",
+              "secondary": "#155e75",
+              "accent": "#155e75",
+              "neutral": "#100e01",
+              "base-100": "#fff8d7",
+              "info": "#0369a1",
+              "success": "#0f766e",
+              "warning": "#facc15",
+              "error": "#e11d48",
+            },
+          },
+        ],
+      },
+    },
+    plugins: [daisyui],
+  }
 }
 
 export default config
