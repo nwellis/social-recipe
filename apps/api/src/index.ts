88f50b790d2dbe2805e9a8da0b6447c762010f32
nwellis <nicholas.w.ellis@gmail.com>
2024-04-08 07:08:55 -0500

    feat(api): cors configuration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule.js';
 import cookieParser from 'cookie-parser';
+import { ServerEnv } from '@acme/server-env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser);
+
+  if (ServerEnv.Server.CORS.enabled) {
+    app.enableCors({
+      origin: ServerEnv.Server.CORS.origin,
+    })
+  }
   
   await app.listen(3000);
 }
