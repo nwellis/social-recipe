6cd336bec4e991494e3f7ca6b16156f2fd4d60ac
nwellis <nicholas.w.ellis@gmail.com>
2024-04-12 15:51:21 -0500

    feat(api): api versioning

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,10 +3,16 @@ import { AppModule } from './AppModule.js';
 import cookieParser from 'cookie-parser';
 import { ServerEnv } from '@acme/server-env';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  })
 
   if (ServerEnv.Server.CORS.enabled) {
     console.debug(`CORS ENABLED`, ServerEnv.Server.CORS.origin)
