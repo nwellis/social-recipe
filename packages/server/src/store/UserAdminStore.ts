ce6d39f66556bb9e89e76899e990301e4ba1834a
nwellis <nicholas.w.ellis@gmail.com>
2024-04-13 12:00:02 -0500

    fix(server): unique constraint on user email

--- packages/server/src/store/UserAdminStore.ts
+++ packages/server/src/store/UserAdminStore.ts
@@ -3,5 +3,13 @@ import { MongoEntityStore } from "../db/MongoEntityStore.js";
 import { CollectionOptions } from "../db/EnsureMongoCollection.js";
 
 export const UserAdminStore: DatabaseEntityStore<UserAdmin> = new MongoEntityStore(
-  new CollectionOptions<UserAdmin>("user-admin", "_id", []),
+  new CollectionOptions<UserAdmin>("user-admin", "_id", [
+    {
+      name: "_email_",
+      unique: true,
+      key: {
+        email: 1
+      }
+    },
+  ]),
 )
--- packages/server/src/store/UserCustomerStore.ts
+++ packages/server/src/store/UserCustomerStore.ts
@@ -3,5 +3,13 @@ import { MongoEntityStore } from "../db/MongoEntityStore.js";
 import { CollectionOptions } from "../db/EnsureMongoCollection.js";
 
 export const UserCustomerStore: DatabaseEntityStore<UserCustomer> = new MongoEntityStore(
-  new CollectionOptions<UserCustomer>("user-customer", "_id", []),
+  new CollectionOptions<UserCustomer>("user-customer", "_id", [
+    {
+      name: "_email_",
+      unique: true,
+      key: {
+        email: 1
+      }
+    },
+  ]),
 )
