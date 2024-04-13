2c814943879c5cc32eba9ec5c703ed93819a8db1
nwellis <nicholas.w.ellis@gmail.com>
2024-04-13 07:31:53 -0500

    fix(api): app module setup

--- apps/api/src/AppModule.ts
+++ apps/api/src/AppModule.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './domain/auth/AuthController.js';
-import { UserController } from './domain/user/UserController.js';
-import { MonitorController } from './domain/monitor/MonitorController.js';
 import { AuthModule } from './domain/auth/AuthModule.js';
 import { UserModule } from './domain/user/UserModule.js';
 import { MonitorModule } from './domain/monitor/MonitorModule.js';
@@ -11,11 +8,6 @@ import { MonitorModule } from './domain/monitor/MonitorModule.js';
     AuthModule,
     MonitorModule,
     UserModule,
-  ],
-  controllers: [
-    AuthController,
-    MonitorController,
-    UserController,
   ],
   providers: [],
 })
