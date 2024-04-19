be4bf9c57cebe08b4f84e9d439e2c2c01abc9252
nwellis <nicholas.w.ellis@gmail.com>
2024-04-19 06:32:10 -0500

    feat(core): recipe with slug

--- packages/core/src/model/Recipe.ts
+++ packages/core/src/model/Recipe.ts
@@ -4,6 +4,7 @@ export interface Recipe extends ServerEntityManaged {
   updatedAt: number
 
   orgId: string
+  slug: string
   name: string
   description: string
   ingredients: RecipeIngredient[]
