5a422596f46b3e712d8952ff5f6bc4c20d3c7421
nwellis <nicholas.w.ellis@gmail.com>
2024-04-21 11:48:29 -0500

    feat(ui): tw and daisy integrate

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import sharedConfig from "@acme/css-config/tailwind.config.js";
+import daisyui from 'daisyui';
+
+const fileExtensions = 'html,js,jsx,md,mdx,ts,tsx' as const
+
+const config: Config = {
+  presets: [sharedConfig],
+  content: [
+    `./src/**/*.{${fileExtensions}}`
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [daisyui],
+}
+
+export default config
