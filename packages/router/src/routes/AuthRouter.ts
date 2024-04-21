9635a61a823d964e9d8ec0afce639108e9d0ac04
nwellis <nicholas.w.ellis@gmail.com>
2024-04-21 11:10:34 -0500

    feat(router): invalidate session

--- packages/router/src/routes/AuthRouter.ts
+++ packages/router/src/routes/AuthRouter.ts
@@ -1,8 +1,15 @@
 import { publicProcedure } from "../TRPC.js";
 import { t } from "../TRPC.js";
+import { lucia } from '../lib/Lucia.js';
 
 export const authRouter = t.router({
   getSession: publicProcedure.query(({ ctx }) => {
     return (ctx.session?.expiresAt?.valueOf() ?? 0) > Date.now() ? ctx.session : undefined
+  }),
+  deleteSession: publicProcedure.query(async ({ ctx }) => {
+    if (!ctx.session?.id) {
+      return
+    }
+    await lucia.invalidateSession(ctx.session.id)
   }),
 })
