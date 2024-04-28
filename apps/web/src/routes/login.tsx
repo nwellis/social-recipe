29467e14bdeaf67aef282e03ef7aa3b2477a71fb
nwellis <nicholas.w.ellis@gmail.com>
2024-04-28 07:35:57 -0500

    style(web): login page with daisy theme

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -13,7 +13,7 @@ function Login() {
     <div className="h-full flex flex-col justify-center items-center">
       <h1 className='font-bold text-3xl text-primary'>Recipes</h1>
 
-      <div className="card w-96 bg-white shadow-xl">
+      <div className="card bg-neutral text-neutral-content w-96 shadow-xl">
         <div className="card-body flex flex-col gap-2">
           <h2 className="card-title">Login</h2>
           <p>Login with one of the following</p>
