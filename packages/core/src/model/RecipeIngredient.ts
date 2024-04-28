d8d7d3f15f801419cdfa1a2daf04a53cc110614d
nwellis <nicholas.w.ellis@gmail.com>
2024-04-28 09:25:48 -0500

    doc(core): possible ingredient data source

--- packages/core/src/model/RecipeIngredient.ts
+++ packages/core/src/model/RecipeIngredient.ts
@@ -1,6 +1,8 @@
 import { ServerEntityManaged } from "../index.js";
 import type { MeasurementUnit } from "./MeasurementUnit.js";
 
+// https://www.ars.usda.gov/northeast-area/beltsville-md-bhnrc/beltsville-human-nutrition-research-center/methods-and-application-of-food-composition-laboratory/mafcl-site-pages/sr11-sr28/
+// https://www.ars.usda.gov/ARSUserFiles/80400525/Data/SR27/asc/FOOD_DES.txt
 export interface RecipeIngredient extends ServerEntityManaged {
   recipeId: string
   ingredientId: string
